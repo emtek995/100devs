b329095d80ef81ba4054e2ccf76a81ae5ece809e
Ed Way <emtek995@gmail.com>
2022-02-19 09:14:11 -0600

    changed some js

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,19 +1,25 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+document.getElementById('purple').addEventListener('click',
+  () => document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)', false);
+document.getElementById('green').addEventListener('click',
+  () => document.querySelector('body').style.backgroundColor = 'rgba(0, 253, 81, 1)', false);
+document.getElementById('blue').addEventListener('click',
+  () => document.querySelector('body').style.backgroundColor = 'rgba(0,255,255,1)', false);
 
+// document.getElementById('purple').onclick = partyPurple
+// document.getElementById('green').onclick = partyGreen
+// document.getElementById('blue').onclick = partyBlue
 
-function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
-}
+// function partyPurple() {
+//   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+//   document.querySelector('body').style.color = 'white'
+// }
 
-function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
-}
+// function partyGreen() {
+//   document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+//   document.querySelector('body').style.color = 'white'
+// }
 
-function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'white'
-}
+// function partyBlue() {
+//   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+//   document.querySelector('body').style.color = 'white'
+// }
--- class12-materials/homework/README.md
+++ class12-materials/homework/README.md
@@ -5,4 +5,7 @@
 - Read: https://javascript.info/variables + Tasks
 - Read: https://javascript.info/function-basics + Tasks
 - Do: Delete the JS and do it again for all assignments
+    - add color
+    - add number
+    - case insensative
 - Do: Something special for yourself this weekend
